00cc39341adb6a760907d96ba89bf369619b1448
Aysha Sabah <151834981+AyshaSabah@users.noreply.github.com>
2025-09-14 13:35:57 +0530

    Add files via upload

---
+++ Simple Interest Calculator/scalculator.js
@@ -0,0 +1,7 @@
+let principal = 1000; // a. Principal amount
+let rate = 5;         // b. Interest rate (in %)
+let time = 2;         // c. Time in years
+
+let simpleInterest = (principal * rate * time) / 100; // d. Calculation
+
+console.log("Simple Interest is: $" + simpleInterest);
